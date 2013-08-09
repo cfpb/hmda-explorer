d52a789e5b365189cd822ad5b5c747e8b339ff26
Theresa Summa <theresa.summa@cfpb.gov>
2013-08-09 17:17:54 -0300

    number formatting

--- src/static/js/modules/utils.js
+++ src/static/js/modules/utils.js
@@ -157,6 +157,10 @@ var PDP = (function( pdp ) {
     };
   }
 
+  String.prototype.splice = function( idx, rem, s ) {
+      return (this.slice(0,idx) + s + this.slice(idx + Math.abs(rem)));
+  };
+
   return pdp;
 
 }( PDP || {} ));
