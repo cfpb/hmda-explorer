6f1210bf19752baea5ac79e9c9797c4031ed4a5c
Chris Contolini <christopher.contolini@cfpb.gov>
2013-08-30 10:24:21 -0400

    Reverse the orderBy elements so things sort properly

--- dist/static/js/modules/query.js
+++ dist/static/js/modules/query.js
@@ -274,7 +274,7 @@ var PDP = (function ( pdp ) {
     },
 
     group: function( param ) {
-      return '&$group=' + this._listVals( param ) + '&$orderBy=' + this._listVals( param );
+      return '&$group=' + this._listVals( param ) + '&$orderBy=' + this._listVals( param.reverse() );
     },
 
     // formats single value clauses
