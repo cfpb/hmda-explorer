8cf914b9c6b4d45917fd86c632c6ac8d15d83a71
Andrew <awolfe76@gmail.com>
2015-06-10 13:34:27 -0400

    added tech data spec and handlebars template; following the pattern for js added a spec.js

--- src/static/js/pages/spec.js
+++ src/static/js/pages/spec.js
@@ -2,8 +2,21 @@ $(function(){
 
   'use strict';
 
-  $.getJSON('static/json/technical_data_specification.json', function(result){
-    console.log(result);
+  var source;
+
+  $.ajax({
+    async: false,
+    url: 'static/js/hbs/spec.html',
+    dataType: 'html',
+    success: function(data) {
+      source = data;
+    }
+  });
+
+  var template = Handlebars.compile(source);
+
+  $.getJSON('static/json/technical_data_specification.json', function(data) {
+    $('#spec').html(template(data));
   });
 
 });
